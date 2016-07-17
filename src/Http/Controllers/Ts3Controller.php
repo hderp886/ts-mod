1bf46b3f657c1cc4574e9a7c93498ecb69c4013c
hderp886 <hderp886@gmail.com>
2016-07-17 19:33:18 +0100

    base

---
+++ src/Http/Controllers/Ts3Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Seat\Ts3\Http\Controllers;
+
+class Ts3Controller extends Controller
+{
+    public function getControls()
+    {
+        //return view('controls');
+        return 'Hello World';
+    }
+    
+}
---
+++ src/Http/routes.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::group([
+    'namespace' => 'Seat\Ts3\Http\Controllers',
+    'prefix' => 'teamspeak'
+], function(){
+        
+    Route::get('/', [
+        'as' => 'ts3.home',
+        'uses' => 'Ts3Controller@getControls'
+    ]);
+    
+});
+
+?>
